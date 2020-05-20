c2eff4c5c736430802976cbc24a05a5355bee516
solicks <solicks8355@gmail.com>
2020-05-20 07:56:56 +0100

    Updates

---
+++ precache-manifest.19f99f07b9529e847a44d754c50085cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8554ce328d7ef0700c57b9848f59dd9a",
+    "url": "/mytodolist/index.html"
+  },
+  {
+    "revision": "e71a4443e8b4649b8645",
+    "url": "/mytodolist/static/css/main.2c5508fd.chunk.css"
+  },
+  {
+    "revision": "ba5ca40767c379b52979",
+    "url": "/mytodolist/static/js/2.5f586fe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodolist/static/js/2.5f586fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a4443e8b4649b8645",
+    "url": "/mytodolist/static/js/main.73e5e693.chunk.js"
+  },
+  {
+    "revision": "f99927bfb58eed1081c5",
+    "url": "/mytodolist/static/js/runtime-main.0061c66c.js"
+  }
+]);
